433a6a55a0ad008d4ab1ab3cb0bfe890dbbb36ec
Scott Brady <d00351420@dmail.dixie.edu>
2019-11-08 12:57:11 -0700

    keypad added

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var page = document.querySelector("wrapper");
+import Pad from '/pad.js';
+
+//var page = document.querySelector("wrapper");
+let canvas = document.getElementById("gameScreen");
+
+let context = canvas.getContext("2d");
+
+const GAME_WIDTH = 1500;
+const GAME_HEIGHT = 700;
+
+// Clearing the screen
+//ontext.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+let keypad = new Pad(GAME_WIDTH, GAME_HEIGHT);
+
+keypad.draw(context);
---
+++ pad.js
@@ -0,0 +1,16 @@
+export default class Pad {
+    constructor(gameWidth, gameHeight) {
+        this.width = 100;
+        this.height = 100;
+        this.position = {
+            x: gameWidth / 2 - this.width / 2,
+            y: gameHeight - this.height - 10
+        };
+    }
+    
+    draw(context) {
+        //context.fillStyle = '#f00';
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+    
+}
