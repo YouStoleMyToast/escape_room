be50de9aa62675870bc86d315d4f6fbbec3f15b0
dillanj <dillan.johnson1@dmail.dixie.edu>
2019-11-08 23:15:12 -0700

    image loading dynamically

--- app.js
+++ app.js
@@ -1,22 +1,69 @@
 import Pad from './pad.js';
+import GameScreen, { Door } from './gamescreen.js';
+
 const GAME_WIDTH = 960;
 const GAME_HEIGHT = 640;
 
-//var page = document.querySelector("wrapper");
+var wordObject;
+
 var GameCanvas = document.createElement("canvas");
 GameCanvas.id = "gameScreen";
 GameCanvas.width = GAME_WIDTH;
 GameCanvas.height = GAME_HEIGHT;
+var GameContext = GameCanvas.getContext("2d");
+
+
 
 var loadGameScreen = () => {
     document.querySelector("wrapper").appendChild(GameCanvas);
 }
 
-loadGameScreen();
-var GameContext = GameCanvas.getContext("2d");
-let keypad = new Pad(GAME_WIDTH, GAME_HEIGHT);
+
+var getWord = function(){
+    fetch(`http://localhost:8080/getWord`).then( function ( response ){
+        response.json().then( function ( data ){
+            console.log("The data is: ", data);
+            loadGameScreen();
+            wordObject = data;
+            loadImage();
+            var colors = [/*light grey*/'#BBB8AF',/*dark brown*/'#3A190F',/*light brown*/'#653D31'];
+            let gamescreen = new GameScreen(GAME_WIDTH, GAME_HEIGHT);
+            gamescreen.loadObjects(); 
+            gamescreen.draw(GameContext, colors, wordObject);
+        });
+    });
+};
+
+getWord();
+
+var loadImage = function () {
+    var word_image = document.createElement("img");
+    GameCanvas.appendChild(word_image); 
+};
 
 
+
+
+
+
+
+
+
+
+
+
+
+// function getClickPosition(e){
+//     var mouse_x = e.clientX;
+//     var mouse_y = e.clientY;
+// }
+// keypad.addEventListener("click",getClickPosition,false)
+
+
+// Clearing the screen
+//ontext.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+/*let keypad = new Pad(GAME_WIDTH, GAME_HEIGHT);
 keypad.loadOptionBlocks();
 keypad.loadInputBlocks();
-keypad.draw(GameContext);
+keypad.draw(GameContext);*/
